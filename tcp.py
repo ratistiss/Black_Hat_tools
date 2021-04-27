cfdce15c5f077dcf6a1eec740561a8d559a10a56
ratistiss <50683985+ratistiss@users.noreply.github.com>
2021-04-27 09:13:38 -0400

    tcp client finished

--- tcp.py
+++ tcp.py
@@ -8,3 +8,9 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #connect to the client
 client.connect((target_host,target_port))
+
+#data send
+client.send("GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
+
+#data rec
+response = client.recv(4096)
