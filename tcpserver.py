3277062545b3a48647b5a05101104d9b9a45b740
ratistiss <50683985+ratistiss@users.noreply.github.com>
2021-04-29 06:10:51 -0400

    tcp server part2

--- tcpserver.py
+++ tcpserver.py
@@ -10,3 +10,27 @@ server.bind((bind_ip, bind_port))
 
 server.listen(5)
 
+print "[*] Listening on %s:%d" % (bind_ip,bind_port)
+
+#client handling thread
+def handle_client(client_socket):
+    #printing what client sends
+    request = client_socket.recv(1024)
+
+    print "[*] Listening on %s" % request
+
+    #returning a packet
+    client_socket.send("your response")
+
+    client_socket.close()
+
+
+while True:
+    client,addr = server.accept()
+
+    print "[*] Accepted connection from:  %s:%d" % (addr[0],addr[1])
+
+    #start thread to handle incoming data
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
+
