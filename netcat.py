3c4f89e49be1e89104decd0b8fc040f49d820362
ratistiss <50683985+ratistiss@users.noreply.github.com>
2021-05-03 06:18:20 -0400

    netcat build part 5

--- netcat.py
+++ netcat.py
@@ -46,7 +46,23 @@ def main():
         print str(err)
         usage()
 
-    for 0,a in opts:
+    for o,a in opts:
+        if o in ("-h","--help"):
+            usage()
+        elif o in ("-l","--listen"):
+            listen = True
+        elif o in ("e", "--execute"):
+            execute = a
+        elif o in ("-c", "--commandshell"):
+            command = True
+        elif o in ("-u", "--upload"):
+            upload_destination = a
+        elif o in ("-t", "--target"):
+            target = a
+        elif o in ("-p", "--port"):
+            port = int(a)
+        else:
+            assert False, "Unhandled Option"
     
 
 
