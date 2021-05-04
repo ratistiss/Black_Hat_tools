e994d216f08c1eb95feda1da9b0cd024b3ebe8fc
ratistiss <50683985+ratistiss@users.noreply.github.com>
2021-05-04 06:11:47 -0400

    netcat client sender function

--- netcat.py
+++ netcat.py
@@ -28,6 +28,41 @@ def usage():
     print "echo 'AbCdEfGhI' | ./bhpnet.py -t 192.168.11.12 -p 135"
     sys.exit(0)
 
+
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        #connect to client
+        client.connect((target,port))
+
+        if len(buffer):
+            client.send(buffer)
+
+        while True:
+            recv_len = 1
+            response = ""
+
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data
+
+                if recv_len < 4096:
+                    break
+
+            print response,
+
+            buffer = raw_input("")
+            buffer += "\n"
+            client.send(buffer)
+    
+    except:
+        print "[*] Exception! Exiting."
+
+        client.close()
+            
+
 def main():
     global listen
     global port
