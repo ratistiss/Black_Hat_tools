f0270a9b204759a8d2107c9fea37df2508da1564
ratistiss <50683985+ratistiss@users.noreply.github.com>
2021-05-07 08:19:46 -0400

    client handler finished

--- netcat.py
+++ netcat.py
@@ -136,6 +136,9 @@ def client_handler(client_socket):
 
             #send command output back
             response = run_command(cmd_buffer)
+
+            #send back the response
+            client_socket.send(response)
             
 
     
