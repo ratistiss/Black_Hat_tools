9306998ac861c2b8ecc234a88f84c4e9e0826d03
ratistiss <50683985+ratistiss@users.noreply.github.com>
2021-05-11 06:08:52 -0400

    TCP proxy server_loop func

--- tcpproxy.py
+++ tcpproxy.py
@@ -2,3 +2,11 @@ import sys
 import socket
 import threading
 
+def server_loop(local_host, local_port, remote_host, remote_port,receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        server.bind((local_host,local_port))
+    except:
+        print("Failed to listen")
+        sys.exit(0)
